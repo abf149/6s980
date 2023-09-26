294cbabd5b70ed851ac0b409f8052bb31d7ebc99
Andrew Feldman <afeld2012@gmail.com>
2023-09-26 00:27:31 -0400

    rendering works

--- 6S980-hw1/src/rendering.py
+++ 6S980-hw1/src/rendering.py
@@ -1,87 +1,7 @@
 from jaxtyping import Float
-from torch import (
-    Tensor,
-    ones_like,
-    float32,
-    sum as torch_sum,
-    inverse,
-    cat,
-    ones,
-    zeros,
-    matmul,
-)
+from torch import Tensor, arange, ones, round
 
-import torch
-
-# from .geometry import homogenize_points, project, transform_world2cam
-
-
-def homogenize_points(
-    points: Float[Tensor, "*batch dim"],
-) -> Float[Tensor, "*batch dim+1"]:
-    """Turn n-dimensional points into (n+1)-dimensional homogeneous points."""
-
-    # Homogenous points require fourth coordinate == 1 to allow displacement
-    return cat(
-        [points, ones(*points.shape[:-1], 1, dtype=points.dtype, device=points.device)],
-        dim=-1,
-    )
-
-
-def homogenize_vectors(
-    points: Float[Tensor, "#batch dim"],
-) -> Float[Tensor, "*batch dim+1"]:
-    """Turn n-dimensional vectors into (n+1)-dimensional homogeneous vectors."""
-
-    # Homogenous vectors require fourth coordinate == 0 to avoid displacement
-    return cat(
-        [
-            points,
-            zeros(*points.shape[:-1], 1, dtype=points.dtype, device=points.device),
-        ],
-        dim=-1,
-    )
-
-
-def transform_rigid(
-    xyz: Float[Tensor, "*#batch 4"],
-    transform: Float[Tensor, "*#batch 4 4"],
-) -> Float[Tensor, "*batch 4"]:
-    """Apply a rigid-body transform to homogeneous points or vectors."""
-
-    return matmul(transform, xyz.unsqueeze(-1)).squeeze(-1)
-
-
-def transform_world2cam(
-    xyz: Float[Tensor, "*#batch 4"],
-    cam2world: Float[Tensor, "*#batch 4 4"],
-) -> Float[Tensor, "*batch 4"]:
-    """Transform points or vectors from homogeneous 3D world coordinates to homogeneous
-    3D camera coordinates.
-    """
-
-    return transform_rigid(xyz, inverse(cam2world))
-
-
-def transform_cam2world(
-    xyz: Float[Tensor, "*#batch 4"],
-    cam2world: Float[Tensor, "*#batch 4 4"],
-) -> Float[Tensor, "*batch 4"]:
-    """Transform points or vectors from homogeneous 3D camera coordinates to homogeneous
-    3D world coordinates.
-    """
-
-    return transform_rigid(xyz, cam2world)
-
-
-def project(
-    xyz: Float[Tensor, "*#batch 4"],
-    intrinsics: Float[Tensor, "*#batch 3 3"],
-) -> Float[Tensor, "*batch 2"]:
-    """Project homogenized 3D points in camera coordinates to pixel coordinates."""
-
-    proj = matmul(intrinsics, xyz[..., :3].unsqueeze(-1)).squeeze(-1)
-    return proj[..., :2] / proj[..., 2:3]
+from .geometry import homogenize_points, project, transform_world2cam
 
 
 def render_point_cloud(
@@ -99,22 +19,42 @@ def render_point_cloud(
     vertices_homogeneous = homogenize_points(vertices)
 
     # Transform the vertices into camera space
-    vertices_cam = transform_world2cam(vertices_homogeneous, extrinsics)
+    vertices_cam = transform_world2cam(
+        vertices_homogeneous.unsqueeze(0), extrinsics.unsqueeze(1)
+    )
 
     # Project the transformed vertices onto the image plane
-    projected_points = project(vertices_cam, intrinsics)
+    projected_points = project(vertices_cam, intrinsics.unsqueeze(1))
 
     # Create a white canvas
     batch_size = extrinsics.shape[0]
-    canvas = ones_like(
-        (batch_size, resolution[1], resolution[0]), device=vertices.device
-    )
+    canvas = ones((batch_size, resolution[1], resolution[0]), device=vertices.device)
 
     # Convert projected points to pixel indices and set the corresponding pixel to black
-    x_indices = round(projected_points[..., 0]).long().clamp(0, resolution[0] - 1)
-    y_indices = round(projected_points[..., 1]).long().clamp(0, resolution[1] - 1)
+    x_indices = (
+        round(projected_points[..., 0] * (resolution[0] - 1))
+        .long()
+        .clamp(0, resolution[0] - 1)
+    )
+    y_indices = (
+        round(projected_points[..., 1] * (resolution[1] - 1))
+        .long()
+        .clamp(0, resolution[1] - 1)
+    )
 
-    for i in range(batch_size):
-        canvas[i, y_indices[i], x_indices[i]] = 0
+    # Create a batch index tensor and expand it to match the shape of x_indices and
+    # y_indices
+    batch_indices = (
+        arange(batch_size, device=vertices.device).unsqueeze(1).expand_as(x_indices)
+    )
+
+    # Flatten the tensors for 1D indexing
+    x_indices_flat = x_indices.flatten()
+    y_indices_flat = y_indices.flatten()
+    batch_indices_flat = batch_indices.flatten()
+
+    # Use advanced indexing to set the canvas to zero for every batch and every
+    # projected vertex
+    canvas[batch_indices_flat, y_indices_flat, x_indices_flat] = 0
 
     return canvas
